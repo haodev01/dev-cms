f51dcd1332c3be36c50dde2a9fb47fc969cd8000
haonc <haonc@vega.com.vn>
2024-06-12 18:33:10 +0700

    Init

--- routes/admin/guest.php
+++ routes/admin/guest.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/login', function () {
   return view('admin.pages.login');
 });
+Route::get('/register', function () {
+  return view('admin.pages.register');
+});
